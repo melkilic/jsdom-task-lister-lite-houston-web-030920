e56a7b58d4546677bc96a57ad9c9d3651c5129b7
melkilic <melike180400@gmail.com>
2020-04-30 22:50:44 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+let prioritySelect = document.createElement('select')
+let high = document.createElement('option')
+high.innerHTML = "High"
+let medium = document.createElement('option')
+medium.innerHTML = "Medium"
+let low = document.createElement('option')
+low.innerHTML = "Low"
+prioritySelect.append(high,medium,low)
+
+let form = document.querySelector("#create-task-form")
+form.append(prioritySelect)
+
+document.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let userTask = document.querySelector('#new-task-description').value
+  let priorityValue = document.querySelector('select').value
+  let task = document.createElement('li')
+  if (priorityValue == "High Priority !!!"){
+    task.style.color = "red"
+  } else if (priorityValue == "Medium Priority !!"){
+    task.style.color = "yellow"
+  }else if (priorityValue == "Low Priority !"){
+    task.style.color = "green"
+  }
+  let btn = document.createElement('button')
+  btn.innerHTML = 'X'
+  task.innerHTML = userTask
+  task.append(btn)
+  
+  let ulTodos = document.getElementById('tasks');
+  ulTodos.append(task)
+  btn.addEventListener('click', function(){
+    task.remove()
+  })
 });
+
